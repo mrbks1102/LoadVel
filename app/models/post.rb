4f0d203f8e74ddd09ebb8fafe029fa0ed71c8840
mrbks1102 <mrbks1102@gmail.com>
2020-05-20 19:07:26 +0900

    [add]postモデルのバリデーションを追加 #22

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :post_photo, presence: true
 end
--- db/migrate/20200520094231_create_posts.rb
+++ db/migrate/20200520094231_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :post_photo
+      t.string :post_photo, null: false
       t.references :user, foreign_key: true, null: false
       t.timestamps
     end

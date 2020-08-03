2ff9dfa32ae5603e9502cc88cd3b0f2d5f06b084
mrbks1102 <mrbks1102@gmail.com>
2020-08-03 20:43:21 +0900

    [modify]pagesをtoppagesに変更 #38

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: 'pages#index'
+  root to: 'toppages#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'

f0c612086985d33eee332d752f86caf63a86a4e8
mrbks1102 <mrbks1102@gmail.com>
2020-08-17 20:58:48 +0900

    [modify]scss修正+不要ファイル削除 #38 #60

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,11 +1,11 @@
 $(document).on("turbolinks:load", function () {
-  $(".nav_item").click(function () {
-    $(".nav_item").removeClass("active");
+  $(".user_nav_item").click(function () {
+    $(".user_nav_item").removeClass("active");
     $(this).addClass("active");
 
-    var index = $(".nav_item").index(this);
-    $(".user_contents_item").eq(index).addClass("activ");
-    $(".user_contents_item").eq(index).siblings().removeClass("activ");
+    var index = $(".user_nav_item").index(this);
+    $(".user_contents_item").eq(index).addClass("item_active");
+    $(".user_contents_item").eq(index).siblings().removeClass("item_active");
   });
 });
 

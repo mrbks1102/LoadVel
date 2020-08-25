eab347f3f401dfa38642c12bf10f2b565260f9c1
mrbks1102 <mrbks1102@gmail.com>
2020-08-25 20:05:30 +0900

    [add][modify]aboutページ作成+footerレイアウト修正 #38 #54 #71

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
   resources :searches, only: [:index]
   resources :categories, only: [:index]
+  resources :abouts, only: [:index]
 end

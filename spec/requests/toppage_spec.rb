058443697e14ec52a5534dff7ea84b333b419d03
mrbks1102 <mrbks1102@gmail.com>
2020-09-06 01:27:23 +0900

    [add]request toppage テスト追加 #53

---
+++ spec/requests/toppage_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Toppages", type: :request do
+  describe "index" do
+    before { get root_path }
+
+    example 'リクエストが成功すること' do
+      expect(response).to have_http_status(200)
+    end
+  end
+end

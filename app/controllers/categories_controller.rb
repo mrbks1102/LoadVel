f2022b196181ad98cd7c71c545718725ce02fb11
mrbks1102 <mrbks1102@gmail.com>
2020-09-10 20:02:55 +0900

    [add][modify]request categories　テスト追加+controller修正 #38 #53

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    @post = Post.order(created_at: :desc)
     @category_id = Category.find(params[:category_id])
-    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category_id })
+    @post = Post.includes(:categories).where(post_category_relations: { category_id: @category_id }).order(created_at: :desc)
   end
 end

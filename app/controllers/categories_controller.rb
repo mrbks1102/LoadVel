12dbe59b41194c1985a0f79eab1992e3e1ac9f29
mrbks1102 <mrbks1102@gmail.com>
2020-09-16 23:04:37 +0900

    [add]ページネーション追加 #89

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def index
     @category_id = Category.find(params[:category_id])
     @post = Post.includes(:categories).where(post_category_relations: { category_id: @category_id }).order(created_at: :desc)
+    @category_post = @post.page(params[:page]).per(4)
   end
 end

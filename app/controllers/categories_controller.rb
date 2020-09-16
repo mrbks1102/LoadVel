b435342c6b77ab1c1a66c09bed8145efe98fbccb
mrbks1102 <mrbks1102@gmail.com>
2020-09-17 00:15:41 +0900

    [modify]ページネーションを非同期通信化 #89

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,10 @@ class CategoriesController < ApplicationController
     @category_id = Category.find(params[:category_id])
     @post = Post.includes(:categories).where(post_category_relations: { category_id: @category_id }).order(created_at: :desc)
     @category_post = @post.page(params[:page]).per(4)
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end

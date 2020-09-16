3725b55dcb083a7a88b3779183d8f9510cc651d9
mrbks1102 <mrbks1102@gmail.com>
2020-09-16 12:22:02 +0900

    [modify]URLテキストをハイパーリンク化 #38

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,15 @@ module ApplicationHelper
     end
     html.html_safe
   end
+
+  require "uri"
+
+  def text_url_to_link(text)
+    URI.extract(text, ['http', 'https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      text.gsub!(url, sub_text)
+    end
+    text
+  end
 end

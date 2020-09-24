bc44da05eaefd097fc1a19b1c8056e3a42fcf1e7
mrbks1102 <mrbks1102@gmail.com>
2020-09-24 16:00:31 +0900

    [add]本番環境設定追加+修正 #84

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,9 +43,8 @@ class User < ApplicationRecord
   end
 
   def self.guest
-    User.find_or_create_by!(email: "guest@example.com") do |user|
+    User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name "ゲストユーザー"
     end
   end
 end
